b558e5f4e9b7ad3740f0817df41e0758d38a4ea7
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2020-07-07 14:54:47 -0400

    tues 3pm stuck on dates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
+require 'pry'
+require 'date'
+
 class User < ActiveRecord::Base
     has_many :activity_logs
     has_many :activity_types, through: :activity_logs
@@ -13,7 +16,7 @@ class User < ActiveRecord::Base
         my_logs.map {|log| log.activity_type}
     end
 
-    #sums risk scores for all of users activity_types
+    #sums risk scores for all of users activity_types - probably moot
     def sum_score
         my_activity_types.sum{|actype| actype.risk_score}
     end
@@ -31,6 +34,31 @@ class User < ActiveRecord::Base
     #sums scores on a given day
     def score_by_date(date)
         activity_type_by_date(date).sum{|actype| actype.risk_score}
+    end
+
+    
+    def avg_score (date)
+        fourteen_days_ago = date -14
+        sum_scores = 0
+    
+        (date..fourteen_days_ago).each do |day|
+            binding.pry
+            #puts score_by_date(day)
+        end
+
+        # until !date > fourteen_days_ago
+        #     if score_by_date(date) == nil 
+        #         sum_scores += 0
+        #     else
+        #         sum_scores += score_by_date(date)
+        #     end
+        #     date - 1
+        # end
+        # avg_score = sum_scores / 14
+        # loop through each day until day = today-14
+        # for each day call score_by_date
+        # and then go back one more day 
+        
     end
 
 end
